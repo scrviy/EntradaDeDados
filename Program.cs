16363c111c4b6acd2050b0db6b912a3ea7a8ed39
scrviy <91909966+scrviy@users.noreply.github.com>
2023-02-07 11:08:42 -0300

    Entrada de dados

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace EntradaDeDados
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            char ch = char.Parse(Console.ReadLine());
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Você digitou \n{n1}");
+            Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(ch);
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura);
+        }
+    }
+}
